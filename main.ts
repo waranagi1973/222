7c68cc52fed461dd8f2b8c78587f16bef0619307
waranagi1973 <85052491+waranagi1973@users.noreply.github.com>
2022-08-11 10:10:27 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,23 @@
 input.onButtonPressed(Button.A, function () {
     basic.showString("HOUGAKU")
     while (!(input.logoIsPressed())) {
+        hougaku = 1
         basic.showNumber(input.compassHeading())
+        if (hougaku == 1) {
+            if (input.pinIsPressed(TouchPin.P0)) {
+                if (0 == input.compassHeading()) {
+                    music.playMelody("C5 C5 - C5 C5 - C5 C5 ", 2000)
+                } else if (randint(355, 5) == input.compassHeading()) {
+                    music.playMelody("F - F - F - F - ", 1000)
+                } else {
+                    if (randint(357, 3) == input.compassHeading()) {
+                        music.playMelody("- A - A - A - A ", 1500)
+                    }
+                }
+            }
+        }
     }
-    break;
+    hougaku = 0
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showString("Lite Mode")
@@ -23,5 +37,9 @@ input.onButtonPressed(Button.B, function () {
     basic.showString("KION/OND")
     basic.showString("" + (input.temperature()))
 })
+let hougaku = 0
 basic.showString("Hello")
 radio.setGroup(1)
+basic.forever(function () {
+	
+})
